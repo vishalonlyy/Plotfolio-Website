f760204744f22f9b8a1779cdeb9a3d6a7c6d7d8d
vishal889 <vishalrajpurohit416@gmail.com>
2022-12-22 15:17:14 +0530

    Files and Folders Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,tsx,jsx}','./Src/components/**/*.{js,ts,jsx,tsx}','./node_modules/flowbite/**/*.js'],
+  content: ['./pages/**/*.{js,ts,tsx,jsx}','./components/**/*.{js,ts,jsx,tsx}','./node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
